9b9fe2fcbbc666071fe178753f5ac2d478a7560b
gillavitesh210-afk <gillavitesh210@gmail.com>
2025-11-11 17:55:18 -0800

    Add files via upload

---
+++ brickbreaker-site/sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'bb-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/og-1200x630.png',
+  './privacy.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE ? caches.delete(k) : null))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(request).then(cached => cached || fetch(request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(request, copy));
+      return resp;
+    }).catch(() => cached))
+  );
+});
